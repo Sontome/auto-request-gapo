a064f9013e8093f3eeabfbc7ecb3e72d296cbba1
Sontome <93179211+Sontome@users.noreply.github.com>
2026-04-20 23:58:33 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,16 +1,16 @@
 $path = "C:\auto-request-gapo"
 
-# Tạo folder
 New-Item -ItemType Directory -Force -Path $path | Out-Null
 
-# Download zip repo
 $zip = "$env:TEMP\repo.zip"
-Invoke-WebRequest "https://github.com/Sontome/auto-request-gapo/archive/refs/heads/main.zip" -OutFile $zip
 
-# Giải nén
+Invoke-WebRequest `
+"https://github.com/Sontome/auto-request-gapo/archive/refs/heads/main.zip" `
+-UseBasicParsing `
+-OutFile $zip
+
 Expand-Archive $zip -DestinationPath $env:TEMP -Force
 
-# Copy vào C:
 Copy-Item "$env:TEMP\auto-request-gapo-main\*" $path -Recurse -Force
 
 cd $path
@@ -19,7 +19,7 @@ Set-Clipboard "chrome://extensions/"
 
 Add-Type -AssemblyName PresentationFramework
 [System.Windows.MessageBox]::Show(
-"Xong rồi .`nĐã copy chrome://extensions/",
+"Xong rồi đại ca 😎",
 "Installer",
 "OK",
 "Information"
